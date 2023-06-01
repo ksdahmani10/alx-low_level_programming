19f7fbfe5d3f22973ff9f09dcd8edf79f57c12b4
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2023-06-01 23:03:53 +0200

    Update 0-main.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -17,11 +17,6 @@ int main(void)
 
     head = &hello;
     new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
     new->str = strdup("Hello");
     new->len = 5;
     new->next = head;
